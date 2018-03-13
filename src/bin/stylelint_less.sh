9cdeabe25f024667282a323b19863c59b03d600a
Oliver Kierepka <mrmoree2@gmail.com>
2018-03-13 12:56:12 +0100

    add stylelint

--- src/bin/stylelint_less.sh
+++ src/bin/stylelint_less.sh
@@ -4,12 +4,38 @@
 OPTIND=1         # Reset in case getopts has been used previously in the shell.
 
 ##### Variables #####
-scriptDirectory=$(dirname "$0")
-findFilesCommand="bash ${scriptDirectory}/find_stylesheets_to_check.sh"
-stylelintCommand="${scriptDirectory}/../../node_modules/stylelint/bin/stylelint.js --config ./.stylelintrc"
-stylelintFixMode=false
+
+scriptDirectory=""
+sniffCommand=""
+stylelintFixCommand=""
+fixMode=false
+findViolationsResult=""
+targetBranch=""
+changedLessFiles=()
 
 ##### Functions #####
+
+function constructor()
+{
+    local stylelintConfig
+    local stylelintIgnore
+
+    # readlink has no -f option with Darwin kernels used in Macs
+    if [ "$(uname -s)" = 'Darwin' ]; then
+        scriptDirectory=$(realpath "$(dirname "$(readlink "$(realpath $0)" || echo "$(echo "$(realpath $0)" | sed -e 's,\\,/,g')")")")
+    else
+        scriptDirectory=$(realpath "$(dirname "$(readlink -f "$(realpath $0)" || echo "$(echo "$(realpath $0)" | sed -e 's,\\,/,g')")")")
+    fi
+
+    stylelintConfig="${scriptDirectory}/../config/stylelint/bin/stylelint.js"
+    stylelintIgnore="${scriptDirectory}/../config/stylelint/.stylelintrc"
+
+    findFilesCommand="bash ${scriptDirectory}/find_files_to_check.sh -s .dontSniffJS -f .less"
+    findParentWithFile="bash ${scriptDirectory}/find_parent_with_file.sh"
+    rootDirectory=$($findParentWithFile -d $scriptDirectory -f composer.lock)
+    sniffCommand="${rootDirectory}/node_modules/stylelint/bin/stylelint.js --config=$stylelintConfig"
+    stylelintFixCommand="${rootDirectory}/node_modules/stylelint/bin/stylelint.js --config=$stylelintConfig --fix --ignore-path=$stylelintIgnore"
+}
 
 function show_help()
 {
@@ -20,61 +46,102 @@ function show_help()
     echo "    -f                   Fix all changed Less Files"
 }
 
-function stylelint()
+function sniff()
 {
-    local parameter=""
-    local branchname=$1
-    local changedStyleFiles=()
-    local fixMode=""
-
-    if [ ! -z "$branchname" ];
+    if [ ! -z "$targetBranch" ];
     then
-        parameter=" -t $branchname"
+        parameter=" -t $targetBranch"
     fi
 
-    changedStyleFiles=$($findFilesCommand$parameter)
+    changedLessFiles=$($findFilesCommand$parameter)
 
-    if [ -z "$changedStyleFiles" ];
+    if [ -z "$changedLessFiles" ];
     then
         echo "No Less-Files to check!"
         exit 0
     fi
 
-    for directory in $changedStyleFiles; do
-        echo "stylelinting $directory"
-        if [[ $stylelintFixMode == 'true' ]]
-        then
-           fixMode=" --fix"
-        fi
-        $stylelintCommand$fixMode $directory
-        stylelintResult=$?
+    echo "########### STYLELINT ###########"
+    find_violations
 
-        if ! [ "$stylelintResult" -eq "0" ]
+    if [ "$findViolationsResult" -ne "0" ] && [ "$fixMode" == "true" ]
+    then
+        echo "########### STYLELINT FIX ###########"
+        echo "Trying to fix violations"
+        fix_violations
+
+        echo "########### STYLELINT FIX ###########"
+        echo "Revalidating code after attempt to fix it"
+        find_violations
+
+        if [ "$findViolationsResult" -ne "0" ]
         then
-            result=$stylelintResult
+            printf "\n\e[31mCouldn't fix all violations :( \e[0m\n"
+        else
+            printf "\n\e[32mFixed all Violations :D \e[0m\n"
+        fi
+    fi
+
+    exit $findViolationsResult
+}
+
+function find_violations()
+{
+    findViolationsResult=""
+    local result=0
+
+    for directory in $changedLessFiles; do
+        echo "StyleLinting $directory"
+        $sniffCommand $directory
+        sniffResult=$?
+        if ! [ "$sniffResult" -eq "0" ]
+        then
+            result=$sniffResult
         fi
     done
 
-    exit $result
+    findViolationsResult="$result"
+}
+
+function fix_violations()
+{
+    local stylelintCommand
+
+    if [ fixMode == 'true' ]
+    then
+        stylelintCommand=$stylelintFixCommand
+    fi
+
+    for directory in $changedLessFiles; do
+        echo "Fixing $directory"
+        $stylelintCommand $directory
+        sniffResult=$?
+    done
 }
 
 ###############
 
-while getopts "ht:f" opt; do
+constructor
+
+while getopts "hft:" opt; do
     case "$opt" in
     h)
         show_help
         exit 0
         ;;
-    t)
-        echo "stylelinting in diff to $OPTARG."
-        stylelint $OPTARG
-        ;;
     f)
-        echo "stylelint fixes your files"
-        stylelintFixMode=true
+        echo "Will run in fix mode"
+        fixMode=true
+        ;;
+    t)
+        echo "Sniffing in diff to $OPTARG."
+        targetBranch=$OPTARG
+        ;;
+    \?)
+        echo "Invalid option: -$OPTARG. Use -h flag for help."
+        exit 1
         ;;
     esac
 done
 
-stylelint
+sniff
