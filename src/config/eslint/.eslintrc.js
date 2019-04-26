0d36062c33e25f15690f2c86651623255fc19362
mrmoree <mrmoree2@gmail.com>
2019-04-26 14:49:44 +0200

    ZOO-1359: Coding Standard Update (#43)
    ZOO-1359: fix eslint config + add package json and lock
    
    ZOO-1359: remove whitespace from eslintrc
    
    ZOO-1359: fix coding standard rule
    
    ZOO-1359: Coding Standard Update
    
    ZOO-1359: fix eslint config + add package json and lock
    
    ZOO-1359: remove whitespace from eslintrc
    
    ZOO-1359 readd peer dependencies manually
    
    ZOO-1359: increment package version number
    
    ZOO-1359: readd peer dependencies
    
    ZOO-1359: update packages + update stylelint to fix vulnerabilities
    
    ZOO-1359: Rebase to master

--- src/config/eslint/.eslintrc.js
+++ src/config/eslint/.eslintrc.js
@@ -6,10 +6,17 @@ module.exports = {
     "plugins": [
         "m99coder"
     ],
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "ecmaFeatures": {
+            "impliedStrict": true
+        }
+    },
     "root": true,
     "env": {
         "browser": true,
         "jquery": true,
+        "es6": true,
         "jasmine": true,
         "node": true,
         "mocha": true,
@@ -54,8 +61,26 @@ module.exports = {
         "no-undef": "off",
         "no-plusplus": ["error", {"allowForLoopAfterthoughts": true}],
         "no-underscore-dangle": "off",
-        "vars-on-top": "off",
-        "m99coder/vars-on-top": [2, {"forStatement": true, "forInStatement": true, "forOfStatement": true}],
-        "wrap-iife": ["error", "inside"]
+        "wrap-iife": ["error", "inside"],
+        "no-alert": "error",
+        "no-empty-function": "error",
+        "no-useless-catch": "error",
+        "no-eval": "error",
+        "no-implied-eval": "error",
+        "no-script-url": "error",
+        "no-useless-call": "error",
+        //es6
+        "arrow-spacing": ["error", { "before": true, "after": true }],
+        "no-confusing-arrow": ["error", {"allowParens": false }],
+        "allow-parens": ["as-needed", { "requireForBlockBody": true }],
+        "no-useless-constructor": "error",
+        "no-dupe-class-members": "error",
+        "no-duplicate-imports": "error",
+        "no-useless-computed-key": "error",
+
+        "no-restricted-properties": "error",
+        "operator-linebreak": "error",
+        "no-nested-ternary": "error",
+        "no-unneeded-ternary": "error"
     }
 };
