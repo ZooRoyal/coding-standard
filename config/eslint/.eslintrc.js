b9ed5c769923b994d27487f41aa9e82617d241e9
rdss-hmorsi <hussein.morsi@rewe-digital.com>
2020-08-17 12:03:59 +0200

    eslint configuration
    - pinned versions of dependencies
    - modified eslintrc to not apply typescript plugin for non-ts files
    - added typescript to dependencies for plugin to work.
    - added test for eslinting .ts files

--- config/eslint/.eslintrc.js
+++ config/eslint/.eslintrc.js
@@ -1,35 +1,37 @@
+/**
+ * eslint config
+ *
+ * @licence REWE Digital GmbH
+ */
+
 module.exports = {
     extends: [
-        "eslint:recommended",
-        "standard",
-        "eslint-config-crockford",
-        "plugin:import/errors",
-        "plugin:import/warnings",
-        "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-        // "prettier/@typescript-eslint",
-        // "plugin:prettier/recommended",
+        'eslint:recommended',
+        'standard'
     ],
-    //plugins: ["m99coder", "standard", "import"], // todo m99coder useful?
     parserOptions: {
-        ecmaVersion: 6,
-        sourceType: "module",
-        ecmaFeatures: {
-            jsx: false,
-            impliedStrict: true,
-        },
-        useJSXTextNode: true,
+        ecmaVersion: 2015,
+        sourceType: 'module'
     },
-    ignorePatterns: [
-        "**/node_modules/*"
+
+    overrides: [
+        {
+            files: '*.ts',
+            plugins: [
+                '@typescript-eslint'
+            ],
+            extends: [
+                'plugin:@typescript-eslint/recommended'
+            ],
+            rules: {
+                '@typescript-eslint/no-unused-vars': 2
+            }
+        }
     ],
     root: true,
     env: {
         browser: true,
-        jquery: true,
-        es6: true,
-        jest: true,
-        node: true,
-        builtin: true,
+        jquery: true
     },
     globals: {
         $: false,
@@ -40,55 +42,71 @@ module.exports = {
         document: true,
         require: true,
         _: true,
+        pageData: true, // tracking object for CMP
+        _satellite: true // Adobe tracking function
     },
+    /**
+     * 0 = turned off
+     * 1 = warning
+     * 2 = error
+     */
     rules: {
-        "new-cap": 2,
-        "no-caller": 2,
-        "no-eq-null": 2,
-        indent: ["error", 4],
-        "linebreak-style": ["error", "unix"],
-        quotes: ["error", "single"],
-        semi: ["error", "always"], // necessary, because we have es5 code
-        "one-var": "off",
-        eqeqeq: ["error", "smart"],
-        curly: "error",
-        "for-direction": "error",
-        "no-tabs": "error",
-        complexity: ["error", 20], // 20 is default
-        "no-undef": "off",
-        "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
-        "no-underscore-dangle": "off",
-        "wrap-iife": ["error", "inside"],
-        "no-alert": "error",
-        "no-empty-function": "error",
-        "no-useless-catch": "error",
-        "no-eval": "error",
-        "no-implied-eval": "error",
-        "no-script-url": "error",
-        "no-useless-call": "error",
-        "vars-on-top": "off",
+        'new-cap': 1,
+        'no-caller': 2,
+        'no-eq-null': 2,
+        indent: [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        quotes: [
+            'error',
+            'single'
+        ],
+        semi: 0,
+        'one-var': 0,
+        eqeqeq: ['error', 'smart'],
+        curly: 0,
+        'for-direction': 0,
+        'no-tabs': 'error',
+        complexity: ['error', 20], // 20 is default
+        'no-undef': 2,
+        'no-plusplus': 0,
+        'no-underscore-dangle': 0,
+        'wrap-iife': ['error', 'any'],
+        'no-alert': 2,
+        'no-empty-function': 2,
+        'no-useless-catch': 2,
+        'no-eval': 2,
+        'no-implied-eval': 2,
+        'no-script-url': 2,
+        'no-useless-call': 2,
+        'vars-on-top': 0,
+        'no-console': 0,
+        'no-implicit-globals': 2,
+        'no-return-assign': 2,
+        'no-unused-expressions': 2,
+        'no-unused-vars': 2,
+        'radix': 2,
+        'no-trailing-spaces': 0,
         // es6
-        "arrow-spacing": ["error", { before: true, after: true }],
-        "no-confusing-arrow": ["error", { "allowParens": false }],
-        "arrow-parens": ["error", "as-needed", { requireForBlockBody: true }],
-        "no-useless-constructor": "error",
-        "no-dupe-class-members": "error",
-        "no-duplicate-imports": "error",
-        "no-useless-computed-key": "error",
-        "no-restricted-properties": "error",
-        "operator-linebreak": "error",
-        "no-nested-ternary": "error",
-        "no-unneeded-ternary": "error",
-        // plugin:standard
-        "standard/object-curly-even-spacing": [2, "either"],
-        "standard/array-bracket-even-spacing": [2, "either"],
-        "standard/computed-property-even-spacing": [2, "even"],
-        "standard/no-callback-literal": [2, ["cb", "callback"]],
-        // plugin:import
-        "import/no-unresolved": [2, { commonjs: true, amd: true }],
-        "import/named": 2,
-        "import/namespace": 2,
-        "import/default": 2,
-        "import/export": 2,
-    },
+        'arrow-spacing': ['error', { before: true, after: true }],
+        'no-confusing-arrow': ['error', { allowParens: false }],
+        'arrow-parens': ['error', 'as-needed', { requireForBlockBody: true }],
+        'no-useless-constructor': 2,
+        'no-dupe-class-members': 2,
+        'no-duplicate-imports': 2,
+        'no-useless-computed-key': 2,
+        'no-restricted-properties': 2,
+        'operator-linebreak': 2,
+        'no-nested-ternary': 2,
+        'no-unneeded-ternary': 2,
+        'standard/object-curly-even-spacing': [2, 'either'],
+        'standard/array-bracket-even-spacing': [2, 'either'],
+        'standard/computed-property-even-spacing': [2, 'even'],
+        'standard/no-callback-literal': [2, ['cb', 'callback']]
+    }
 };
