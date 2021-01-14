9df4680fbe6a5bb4fff2322ee9a09aa49a8453d9
Sebastian Knott <33318992+rdss-sknott@users.noreply.github.com>
2021-01-14 15:31:41 +0100

    Add version switch to ProcessRunner (#102)
    * Add version switch to ProcessRunner
    * Make PHPUnit wait for composer to be installed
    * Mark slow tests as sloooooou
    * downgrade doctrine/annotations

--- tests/Unit/CommandLine/Library/ProcessRunnerTest.php
+++ tests/Unit/CommandLine/Library/ProcessRunnerTest.php
@@ -4,6 +4,8 @@ namespace Zooroyal\CodingStandard\Tests\Unit\CommandLine\Library;
 
 use Hamcrest\MatcherAssert;
 use Hamcrest\Matchers;
+use Mockery;
+use PackageVersions\Versions;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
@@ -68,5 +70,97 @@ class ProcessRunnerTest extends TestCase
     {
         $this->expectException(ProcessFailedException::class);
         $this->subject->runAsProcess('git', 'version\'; ls');
+    }
+
+    public function runAsProcessReturningProcessObjectIsVersionStableDataProvider(): array
+    {
+        return [
+            'as String' => ['v3.5@wubwub', 'ls -la', 'ls -la'],
+            'as Array' => ['v5.5@wubwub', 'ls -la', ['ls', '-la']],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider runAsProcessReturningProcessObjectIsVersionStableDataProvider
+     *
+     * @runInSeparateProcess
+     * @preserveGlobalState  disabled
+     *
+     * @param string $versionString
+     * @param string $commandInput
+     * @param mixed $commandOutput
+     */
+    public function runAsProcessReturningProcessObjectIsVersionStable(
+        string $versionString,
+        string $commandInput,
+        $commandOutput
+    ): void {
+        $overwrittenVersions = Mockery::mock('overload:' . Versions::class);
+        $overwrittenProcess = Mockery::mock('overload:' . Process::class);
+
+        $overwrittenProcess->shouldReceive('setTimeout')->once()
+            ->with(null);
+        $overwrittenProcess->shouldReceive('setIdleTimeout')->once()
+            ->with(60);
+        $overwrittenProcess->shouldReceive('run')->once();
+        $overwrittenProcess->shouldReceive('wait')->once();
+
+        $overwrittenVersions->shouldReceive('getVersion')->once()
+            ->with('symfony/process')->andReturn($versionString);
+        $overwrittenProcess->shouldReceive('__construct')->once()
+            ->with($commandOutput);
+
+        $result = $this->subject->runAsProcessReturningProcessObject($commandInput);
+
+        self::assertInstanceOf(Process::class, $result);
+    }
+
+    public function runAsProcessIsVersionStableDataProvider(): array
+    {
+        return [
+            'as String' => ['v3.5@wubwub', 'ls', '-l', '-a', 'ls -l -a'],
+            'as Array' => ['v5.5@wubwub', 'ls', '-l', '-a', ['ls', '-l', '-a']],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider runAsProcessIsVersionStableDataProvider
+     *
+     * @runInSeparateProcess
+     * @preserveGlobalState  disabled
+     *
+     * @param string $versionString
+     * @param string $commandInput
+     * @param string $commandArgument1
+     * @param string $commandArgument2
+     * @param mixed $commandOutput
+     */
+    public function runAsProcessIsVersionStable(
+        string $versionString,
+        string $commandInput,
+        string $commandArgument1,
+        string $commandArgument2,
+        $commandOutput
+    ): void {
+        $expectedOutput = 'schlurbel';
+        $expectedError = 'wurbel';
+        $overwrittenVersions = Mockery::mock('overload:' . Versions::class);
+        $overwrittenProcess = Mockery::mock('overload:' . Process::class);
+
+        $overwrittenVersions->shouldReceive('getVersion')->once()
+            ->with('symfony/process')->andReturn($versionString);
+
+        $overwrittenProcess->shouldReceive('__construct')->once()->with($commandOutput);
+        $overwrittenProcess->shouldReceive('mustRun')->once()->withNoArgs();
+        $overwrittenProcess->shouldReceive('setTimeout')->once()->with(null);
+        $overwrittenProcess->shouldReceive('wait')->once()->withNoArgs();
+        $overwrittenProcess->shouldReceive('getOutput')->once()->withNoArgs()->andReturn($expectedOutput);
+        $overwrittenProcess->shouldReceive('getErrorOutput')->once()->withNoArgs()->andReturn($expectedError);
+
+        $result = $this->subject->runAsProcess($commandInput, $commandArgument1, $commandArgument2);
+
+        self::assertSame($expectedOutput . PHP_EOL . $expectedError, $result);
     }
 }
