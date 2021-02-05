15c2e2188456a1b36a19f70d65cf13d6acfbe983
Sebastian Knott <33318992+rdss-sknott@users.noreply.github.com>
2021-02-05 14:25:59 +0100

    Add some functions to PHPStan bootstrap (#118)

--- src/main/php/CommandLine/ToolAdapters/PHPStanAdapter.php
+++ src/main/php/CommandLine/ToolAdapters/PHPStanAdapter.php
@@ -1,9 +1,10 @@
 <?php
 
-
 namespace Zooroyal\CodingStandard\CommandLine\ToolAdapters;
 
+use ComposerLocator;
 use DI\Annotation\Injectable;
+use RuntimeException;
 use Symfony\Component\Console\Output\OutputInterface;
 use Zooroyal\CodingStandard\CommandLine\Library\Environment;
 use Zooroyal\CodingStandard\CommandLine\Library\GenericCommandRunner;
@@ -27,14 +28,24 @@ class PHPStanAdapter extends AbstractBlackAndWhitelistAdapter implements ToolAda
     protected $whitelistGlue = ' ';
     /** @var PHPStanConfigGenerator */
     private $phpstanConfigGenerator;
+    /** @var array<string,string> */
+    private array $toolFunctionsFileMapping
+        = [
+            'hamcrest/hamcrest-php' => '/hamcrest/Hamcrest.php',
+            'sebastianknott/hamcrest-object-accessor' => '/src/functions.php',
+            'mockery/mockery' => '/library/helpers.php',
+        ];
+    private string $rootDirectory;
+    private string $phpstanConfigPath;
+    private string $vendorPath;
 
     /**
      * PHPStanAdapter constructor.
      *
-     * @param Environment $environment
-     * @param OutputInterface $output
-     * @param GenericCommandRunner $genericCommandRunner
-     * @param TerminalCommandFinder $terminalCommandFinder
+     * @param Environment            $environment
+     * @param OutputInterface        $output
+     * @param GenericCommandRunner   $genericCommandRunner
+     * @param TerminalCommandFinder  $terminalCommandFinder
      * @param PHPStanConfigGenerator $phpstanConfigGenerator
      */
     public function __construct(
@@ -45,40 +56,73 @@ class PHPStanAdapter extends AbstractBlackAndWhitelistAdapter implements ToolAda
         PHPStanConfigGenerator $phpstanConfigGenerator
     ) {
         $this->phpstanConfigGenerator = $phpstanConfigGenerator;
+        $this->vendorPath = $environment->getVendorPath();
+        $this->rootDirectory = $environment->getRootDirectory();
+        $this->phpstanConfigPath = $environment->getPackageDirectory() . '/config/phpstan/phpstan.neon';
         parent::__construct($environment, $output, $genericCommandRunner, $terminalCommandFinder);
     }
 
-    protected function init()
+    protected function init(): void
     {
-        $vendorPath = $this->environment->getVendorPath();
-        $rootDirectory = $this->environment->getRootDirectory();
-        $phpstanConfig = $this->environment->getPackageDirectory() . '/config/phpstan/phpstan.neon';
+        $this->commands['PHPStanBL'] = 'php ' . $this->vendorPath . '/bin/phpstan analyse --no-progress '
+            . '--error-format=github ' . $this->rootDirectory . ' -c ' . $this->phpstanConfigPath;
 
-        $parameters = $this->phpstanConfigGenerator->addConfigParameters(
-            $this->blacklistToken,
-            $rootDirectory,
-            ['includes' => [$phpstanConfig.'.dist']]
-        );
-        $onTheFlyConfig = $this->phpstanConfigGenerator->generateConfig($parameters);
-        $this->phpstanConfigGenerator->writeConfig($phpstanConfig, $onTheFlyConfig);
-
-        $this->commands['PHPStanBL'] = 'php ' . $vendorPath . '/bin/phpstan analyse --no-progress '
-            . '--error-format=github ' . $rootDirectory . ' -c ' . $phpstanConfig;
-
-        $this->commands['PHPStanWL'] = 'php ' . $vendorPath . '/bin/phpstan analyse --no-progress '
-            . '--error-format=github ' . '-c ' . $phpstanConfig . ' %1$s';
+        $this->commands['PHPStanWL'] = 'php ' . $this->vendorPath . '/bin/phpstan analyse --no-progress '
+            . '--error-format=github ' . '-c ' . $this->phpstanConfigPath . ' %1$s';
     }
 
     /**
      * {@inheritdoc}
      */
-    public function writeViolationsToOutput($targetBranch = '', bool $processIsolation = false) : ?int
+    public function writeViolationsToOutput($targetBranch = '', bool $processIsolation = false): ?int
     {
         $toolShortName = 'PHPStan';
         $prefix = $toolShortName . ' : ';
         $fullMessage = $prefix . 'Running full check';
         $diffMessage = $prefix . 'Running check on diff';
 
+        $this->writeConfigFile();
+
         return $this->runTool($targetBranch, $processIsolation, $fullMessage, $toolShortName, $diffMessage);
+    }
+
+    /**
+     * Writes a custom config file just in time for PHPStan to read.
+     */
+    private function writeConfigFile(): void
+    {
+        $additionalConfigValues = ['includes' => [$this->phpstanConfigPath . '.dist']];
+        $additionalConfigValues = $this->addFunctionFilesToBootstrap($additionalConfigValues);
+
+        $parameters = $this->phpstanConfigGenerator->addConfigParameters(
+            $this->blacklistToken,
+            $this->rootDirectory,
+            $additionalConfigValues
+        );
+        $onTheFlyConfig = $this->phpstanConfigGenerator->generateConfig($parameters);
+        $this->phpstanConfigGenerator->writeConfig($this->phpstanConfigPath, $onTheFlyConfig);
+    }
+
+    /**
+     * Adds function bootstraps to PHPStan config so imported functions won't show up as unknown.
+     *
+     * @param array $additionalConfigValues
+     *
+     * @return array<array<string,string>>
+     */
+    private function addFunctionFilesToBootstrap(array $additionalConfigValues): array
+    {
+        foreach ($this->toolFunctionsFileMapping as $tool => $functionsFile) {
+            try {
+                $toolPath = ComposerLocator::getPath($tool);
+                $additionalConfigValues['parameters']['bootstrapFiles'][] = $toolPath . $functionsFile;
+            } catch (RuntimeException $exception) {
+                $this->output->writeln(
+                    '<info>' . $tool . ' not found. Skip loading ' . $functionsFile . '</info>',
+                    OutputInterface::VERBOSITY_VERBOSE
+                );
+            }
+        }
+        return $additionalConfigValues;
     }
 }
