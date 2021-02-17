eaf2fc106eecc41b74c46602cfffa89a66890135
rdss-hmorsi <hussein.morsi@rewe-digital.com>
2021-02-17 12:21:07 +0100

    added test for badcode scss file

--- tests/System/Stylelint/RunStylelintWithConfig.php
+++ tests/System/Stylelint/RunStylelintWithConfig.php
@@ -9,10 +9,19 @@ use Symfony\Component\Process\Process;
 
 class RunStylelintWithConfig extends TestCase
 {
+
+    protected function setUp(){
+        $process = new Process([
+            'npm',
+            'install'
+        ]);
+
+        $process->mustRun();
+    }
     /**
      * @test
      */
-    public function findViolationsByEslint()
+    public function findViolationsByEslintInLess()
     {
         $process = new Process(
             [
@@ -28,6 +37,28 @@ class RunStylelintWithConfig extends TestCase
         $output = $process->getOutput();
 
         self::assertSame(2, $errorCode);
-        MatcherAssert::assertThat($output, H::containsString('Expected no more than 1 empty line'));
+        MatcherAssert::assertThat($output, H::containsString('Expected indentation of 4 spaces'));
+    }
+
+    /**
+     * @test
+     */
+    public function findViolationsByEslintInScss()
+    {
+        $process = new Process(
+            [
+                __DIR__ . '/../../../node_modules/.bin/stylelint',
+                '--config=' . __DIR__ . '/../../../config/stylelint/.stylelintrc',
+                __DIR__ . '/../fixtures/stylelint/BadCode.scss',
+            ]
+        );
+
+        $process->run();
+        $process->wait();
+        $errorCode = $process->getExitCode();
+        $output = $process->getOutput();
+
+        self::assertSame(2, $errorCode);
+        MatcherAssert::assertThat($output, H::containsString('Expected nesting depth to be no more than 3'));
     }
 }
