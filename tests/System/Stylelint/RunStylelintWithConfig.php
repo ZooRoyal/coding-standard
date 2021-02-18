76430e08a1fc2368d5e3cde00cc681c9903f293d
rdss-hmorsi <52418537+rdss-hmorsi@users.noreply.github.com>
2021-02-18 10:02:28 +0100

    Stylelint anpassung (#121)
    * added css,scss and sass file extensions to stylelint glob pattern
    * removed stylelint order plugin
    * updated stylelint to 13.9.0
    * added stylelint-scss plugin
    * removed unused stylelintignore file

--- tests/System/Stylelint/RunStylelintWithConfig.php
+++ tests/System/Stylelint/RunStylelintWithConfig.php
@@ -11,12 +11,23 @@ use function Amp\ByteStream\buffer;
 
 class RunStylelintWithConfig extends AsyncTestCase
 {
+
+    protected function setUp()
+    {
+        $process = new Process([
+            'npm',
+            'install',
+        ]);
+
+        $process->mustRun();
+    }
+
     /**
      * @test
      * @large
      * @coversNothing
      */
-    public function findViolationsByEslint(): ?Generator
+    public function findViolationsByEslintInLess(): ?Generator
     {
         $process = new Process(
             [
@@ -30,6 +41,74 @@ class RunStylelintWithConfig extends AsyncTestCase
         $exitCode = yield $process->join();
 
         self::assertSame(2, $exitCode);
-        MatcherAssert::assertThat($output, H::containsString('Expected no more than 1 empty line'));
+        MatcherAssert::assertThat($output, H::containsString('Expected indentation of 4 spaces'));
+    }
+    /**
+     * @test
+     * @large
+     * @coversNothing
+     */
+    public function findViolationsByEslintInScss(): ?Generator
+    {
+        $process = new Process(
+            [
+                __DIR__ . '/../../../node_modules/.bin/stylelint',
+                '--config=' . __DIR__ . '/../../../config/stylelint/.stylelintrc',
+                __DIR__ . '/../fixtures/stylelint/BadCode.scss',
+            ]
+        );
+
+        yield $process->start();
+        $output = yield buffer($process->getStdout());
+        $exitCode = yield $process->join();
+
+        self::assertSame(2, $exitCode);
+        MatcherAssert::assertThat($output, H::containsString('Expected nesting depth to be no more than 3'));
+    }
+
+    /**
+     * @test
+     * @large
+     * @coversNothing
+     */
+    public function findViolationsByEslintInSass(): ?Generator
+    {
+        $process = new Process(
+            [
+                __DIR__ . '/../../../node_modules/.bin/stylelint',
+                '--config=' . __DIR__ . '/../../../config/stylelint/.stylelintrc',
+                __DIR__ . '/../fixtures/stylelint/BadCode.sass',
+            ]
+        );
+
+        yield $process->start();
+        $output = yield buffer($process->getStdout());
+        $exitCode = yield $process->join();
+
+        self::assertSame(2, $exitCode);
+        MatcherAssert::assertThat($output, H::containsString('Expected nesting depth to be no more than 3'));
+    }
+
+    /**
+     * @test
+     * @large
+     * @coversNothing
+     */
+    public function findViolationsByEslintInCss(): ?Generator
+    {
+        $process = new Process(
+            [
+                __DIR__ . '/../../../node_modules/.bin/stylelint',
+                '--config=' . __DIR__ . '/../../../config/stylelint/.stylelintrc',
+                __DIR__ . '/../fixtures/stylelint/BadCssCode.css',
+            ]
+        );
+
+        yield $process->start();
+        $output = yield buffer($process->getStdout());
+        $exitCode = yield $process->join();
+
+        self::assertSame(2, $exitCode);
+        MatcherAssert::assertThat($output, H::containsString('Unexpected empty block'));
     }
 }
