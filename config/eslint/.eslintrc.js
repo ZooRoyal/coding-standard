5359d6ab788d2c7305959cd3ba26a0a143e371a2
Marcelle Hövelmanns <marcelle.hoevelmanns@rewe-digital.com>
2022-08-08 17:44:43 +0200

    updated comma-dangle rule

--- config/eslint/.eslintrc.js
+++ config/eslint/.eslintrc.js
@@ -155,8 +155,8 @@
         'comma-dangle': ['error', {
             arrays: 'always-multiline',
             objects: 'always-multiline',
-            imports: 'never',
-            exports: 'never',
+            imports: 'always-multiline',
+            exports: 'always-multiline',
             functions: 'never',
         }],
         'space-before-function-paren': ['error', {
